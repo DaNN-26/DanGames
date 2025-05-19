2532840665682e0b839cf23ec68f86cc9ea06d67
DaNN <idaniil535@gmail.com>
2025-05-19 14:42:19 +0300

    new

---
+++ app/src/main/java/com/example/dangames/view/home/HomeScreen.kt
@@ -0,0 +1,20 @@
+package com.example.dangames.view.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+    Content()
+}
+
+@Composable
+private fun Content() {
+    
+}
+
+@Composable
+@Preview
+private fun Preview() {
+    Content()
+}
